b6c9c796e509e6fb2cef93c48550d77979f7b9d2
Sergio Ocaña Gálvez <sergio.ocana@travelperk.com>
2019-05-05 20:35:09 +0200

    First app

---
+++ react/react-complete-app/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    // return (
+    //   <div className="App">
+    //     <header className="App-header">
+    //       <img src={logo} className="App-logo" alt="logo" />
+    //       <h1 className="App-title">Welcome to React</h1>
+    //     </header>
+    //     <p className="App-intro">Hi! I'm a React app.</p>
+    //   </div>
+    // );
+
+    return React.createElement(
+      "div",
+      { className: "App" },
+      React.createElement("h1", null, "This is a test!")
+    );
+  }
+}
+
+export default App;
