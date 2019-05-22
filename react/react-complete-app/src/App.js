f830db3fc1a2fa6bf805479841e2e0d4985e1d17
Sergio Ocaña Gálvez <sergio@istudio.es>
2019-05-22 20:07:20 +0200

    Up to lesson 69

--- react/react-complete-app/src/App.js
+++ react/react-complete-app/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Person from "./Person";
+import Radium, { StyleRoot } from 'radium';
 
 class App extends Component {
   // Only available on components created as
@@ -57,11 +58,15 @@ class App extends Component {
   render() {
     // Example of inline styles
     const style = {
-      backgroundColor: "white",
-      font: "inherit",
+      backgroundColor: "green",
+      color: "white",
       border: "1px solid blue",
       padding: "8px",
-      cursor: "pointer"
+      cursor: "pointer",
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     let persons = null;
@@ -88,8 +93,23 @@ class App extends Component {
           })}
         </div>
       );
+
+      style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
+
     }
 
+    const classes = []
+    if (this.state.persons.length <= 2) {
+      classes.push('red') 
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold')
+    }
+    
     // Remember always to wrap the JSX of a component
     // inside one root HTML enlement that englobes
     // the rest. This makes sense since we are developing
@@ -104,17 +124,20 @@ class App extends Component {
     // we need to use "className". This is a limitation due to
     // the fact the JavaScript has "class" as a reserved word.
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">Hi! I'm a React app.</p>
-        <button style={style} onClick={this.togglePersonsHandler}>
-          Switch Name
-        </button>
-        {persons}
-      </div>
+      <StyleRoot>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">Hi! I'm a React app.</p>
+          <p className={classes.join(' ')}>This is really working!</p>
+          <button style={style} onClick={this.togglePersonsHandler}>
+            Toggle Names
+          </button>
+          {persons}
+        </div>
+      </StyleRoot>
     );
 
     /*
@@ -130,4 +153,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
--- react/react-complete-app/src/Person/Person.js
+++ react/react-complete-app/src/Person/Person.js
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
 import "./Person.css";
+import Radium from 'radium';
 
 const Person = props => {
+  const style = {
+    '@media(min-width: 500px)': {
+      width: '450px'
+    }
+  }
   /*
     "children" is a reserved attribute that grants access
     to any nodes inside the tag defined by the component.
   */
   return (
-    <div className="Person">
+    <div className="Person" style={style}>
       <p onClick={props.click}>
         I'm a {props.name} and I am{" "}
         <button id={props.uid} onClick={props.doubleAge}>
@@ -21,4 +27,4 @@ const Person = props => {
   );
 };
 
-export default Person;
+export default Radium(Person);
